d955e2af6fce6f568551f0c658c3d558e8494749
frankchen0130 <947118251@qq.com>
2018-02-04 21:08:22 +0800

    v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fklearn',
-    version='0.0.1',
+    version='0.0.2',
     description=(
         'The basic tool of my personal.'
     ),
